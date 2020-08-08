a533ca5d8e4dbec0d3d4a0c620bfeaeae47f1b84
mhpicel <mhpicel@gmail.com>
2020-08-07 20:59:53 -0500

    Completed GET reviews endpoint

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var db = require('../database');
 var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 var port = 8888;
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
-app.post('/reviews', (req, res) => {
-
+app.get('/reviews', (req, res) => {
+  db.Review.find(req.query, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 app.listen(port, () => {
