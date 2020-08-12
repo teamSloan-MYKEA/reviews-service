b1c1d40c4f5ea308c8a6d8fd6ce42b03fbc4cd6f
mhpicel <mhpicel@gmail.com>
2020-08-11 22:47:23 -0500

    Set up initial framework for components
    implemented logic for summary statistics

---
+++ client/components/ReviewList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ReviewList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Filtering</div>
+        <div>Feed</div>
+      </div>
+    );
+  }
+}
+
+export default ReviewList;
