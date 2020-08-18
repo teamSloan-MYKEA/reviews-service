9ee19f7d01c4e653646da6dcb534c92d7144caca
mhpicel <mhpicel@gmail.com>
2020-08-18 12:32:02 -0500

    Added cors to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var db = require('../database');
 var app = express();
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
+app.use(cors());
 var port = 3002;
 
 app.get('/reviews', (req, res) => {
