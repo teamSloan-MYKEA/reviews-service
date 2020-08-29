180fcea4f4d5a2a8e3d4b405ce5e6db2e29c19d6
Owen Arthur <owenarthurdesign@gmail.com>
2020-08-29 00:20:46 -0400

    docker compatibility and json storage

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ app.use('/:id', express.static(__dirname + '/../public'));
 app.get('/:id/reviews/:id', (req, res) => {
   var id = path.basename(req.originalUrl);
   pgdb.getReviewById(id)
-    .then(rows => res.status(200).send(rows))
+    .then(row => {
+      let raw = row[0].reviews;
+      let parsed = JSON.parse(raw);
+      console.log(parsed);
+      res.send(parsed);
+    })
     .catch(err => res.status(500).send('error'));
 });
 
@@ -23,3 +28,30 @@ app.get('/:id/reviews/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var cors = require('cors');
+// var pgdb = require('../database/pg');
+// var path = require('path');
+// var app = express();
+// app.use(express.static(__dirname + '/../public'));
+// app.use(bodyParser.json());
+// app.use(cors());
+// var port = 3002;
+
+// app.use('/:id', express.static(__dirname + '/../public'));
+
+// app.get('/:id/reviews/:id', (req, res) => {
+//   var id = path.basename(req.originalUrl);
+//   pgdb.getReviewById(id)
+//     .then(rows => res.status(200).send(rows))
+//     .catch(err => res.status(500).send('error'));
+// });
+
+
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
