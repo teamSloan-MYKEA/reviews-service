d126ad831a974aa098e13a6d801e7c83b93ff050
Owen Arthur <owenarthurdesign@gmail.com>
2020-09-04 22:17:23 -0400

    final deployment func

--- database/pg/dc-index.js
+++ database/pg/dc-index.js
@@ -17,8 +17,25 @@ const getReviewByIdDC = (id) => {
     const qs = `SELECT * from reviews WHERE productid=${id}`;
     client.query(qs)
       .then(data => resolve(data.rows))
-      .catch(err => { reject(err); } );
+      .catch(err => reject(err));
   });
 };
 
-module.exports = { getReviewByIdDC };
+const postReviewToProductDC = (id, rev) => {
+  return new Promise((resolve, reject) => {
+    const qs = `SELECT * from reviews WHERE productid=${id}`;
+    client.query(qs)
+      .then(data => {
+        let revArray = JSON.parse(data.rows[0].reviews);
+        revArray.push(rev);
+        let newArray = JSON.stringify(revArray);
+        const pqs = `UPDATE reviews SET reviews='${newArray}' WHERE productid=${id};`;
+        client.query(pqs)
+          .then(data => resolve(data))
+          .catch(err => reject(err));
+      })
+      .catch(err => reject(err));
+  });
+};
+
+module.exports = { getReviewByIdDC, postReviewToProductDC };
